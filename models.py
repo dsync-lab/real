572f88c40d41e8d74d150a082183da3b2cc50b95
CipherX <ukovictor8@gmail.com>
2024-03-06 02:36:09 +0100

    completed an Add property feature

--- models.py
+++ models.py
@@ -1,12 +1,10 @@
-from flask_sqlalchemy import SQLAlchemy
-from db import db
 
-app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
+from db import db
 
 
 class Agent(db.Model):
     id = db.Column(db.Integer, primary_key=True)
+    image = db.relationship('AgentImage', backref='agent')
     name = db.Column(db.String(100), nullable=False)
     description = db.Column(db.String(200))
     phone = db.Column(db.String(15), nullable=False)
@@ -15,47 +13,35 @@ class Agent(db.Model):
     instagram = db.Column(db.String(100))
     linkedin = db.Column(db.String(100))
     facebook = db.Column(db.String(100))
-    properties = db.relationship('Property', backref='agent', lazy=True)
-    rent_properties = db.relationship('RentProperty', backref='agent', lazy=True)
+    properties = db.relationship('Property', backref='agent', lazy=True, uselist=False, cascade='all, delete-orphan')
 
-class Property(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(100), nullable=False)
-    price = db.Column(db.String(20), nullable=False)
-    address = db.Column(db.String(200), nullable=False)
-    status = db.Column(db.String(10), nullable=False)
-    area = db.Column(db.Float)
-    beds = db.Column(db.Integer)
-    bathrooms = db.Column(db.Integer)
-    garage = db.Column(db.Integer)
-    property_type = db.Column(db.String(50))
-    agent_id = db.Column(db.Integer, db.ForeignKey('agent.id'), nullable=False)
-    images = db.relationship('PropertyImage', backref='property', lazy=True)
-
-class PropertyImage(db.Model):
+class AgentImage(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(100), nullable=False)
     property_id = db.Column(db.Integer, db.ForeignKey('property.id'), nullable=False)
     image_path = db.Column(db.String(100), nullable=False)
-    
-    
-class RentProperty(db.Model):
+
+class Property(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(100), nullable=False)
-    price = db.Column(db.String(20), nullable=False)
+    price = db.Column(db.Integer, nullable=False)
     address = db.Column(db.String(200), nullable=False)
-    status = db.Column(db.String(10), nullable=False)
+    upload_date = db.Column(db.String(200), nullable=False)
+    property_status = db.Column(db.String(10), nullable=False)
     area = db.Column(db.Float)
-    beds = db.Column(db.Integer)
-    bathrooms = db.Column(db.Integer)
+    bathroom = db.Column(db.Integer)
     garage = db.Column(db.Integer)
     property_type = db.Column(db.String(50))
     agent_id = db.Column(db.Integer, db.ForeignKey('agent.id'), nullable=False)
-    images = db.relationship('RentPropertyImage', backref='rent_property', lazy=True)
+    images = db.relationship('PropertyImage', backref='property', lazy=True, uselist=True)
 
-class RentPropertyImage(db.Model):
+class PropertyImage(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(100), nullable=False)
-    rent_property_id = db.Column(db.Integer, db.ForeignKey('rent_property.id'), nullable=False)
+    upload_date = db.Column(db.String(100), nullable=False)
+    property_id = db.Column(db.Integer, db.ForeignKey('property.id'), nullable=False)
     image_path = db.Column(db.String(100), nullable=False)
+    mimetype = db.Column(db.String(50), nullable=False)
+    
+    
 
--- server.py
+++ server.py
@@ -1,7 +1,27 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, flash
+from models import Property, PropertyImage
+from db import db_init, db
+import secrets
+import os
+from werkzeug.utils import secure_filename
+import mimetypes
+from flask_migrate import Migrate
+from wtforms import StringField, IntegerField, FloatField, FileField, SubmitField
+from flask_wtf import FlaskForm
+from wtforms.validators import DataRequired
+from datetime import datetime
 
 
-app = Flask(__name__, static_url_path='/static')
+app = Flask(__name__, static_url_path='/static', static_folder='static')
+
+
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///test-4.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['SECRET_KEY'] = secrets.token_urlsafe(24)
+app.config['UPLOAD_FOLDER'] = 'static/assets/uploads'
+migrate = Migrate(app, db)
+db_init(app)
+
 
 
 properties = {
@@ -75,19 +95,20 @@ def search_property():
 
     return render_template('index.html')
 
-# @app.route("/view/<int:num>")
-# def debug_page(num):
-    #     return render_template("property-single.html", number=num)
 
 
 
 @app.route("/property-view/<int:property_id>")
 def view_property(property_id):
-    print(f"========{property_id}=========")
-    property_data = properties.get(property_id)
+    property_data = Property.query.get(property_id)
+    images = PropertyImage.query.filter_by(property_id=property_id).all()
+    for image in images:
+        current_image_path = image.image_path
+        formatted_path = current_image_path.split("static/", 1)[1]
+        
+
     if property_data:
-        print(f"========{property_data}=========")
-        return render_template("property-single.html", property_data=property_data, source="Buy", route="property" ) 
+        return render_template("property-single.html", property_data=property_data, source="Buy", route="property", image=image, image_path=formatted_path) 
     else:
         return "Property not found", 404
         
@@ -102,6 +123,113 @@ def view_agent():
 @app.route("/admin")
 def admin():
     return "YOO fuck you ass hole"
+
+
+
+
+@app.route("/property-upload", methods=['GET', 'POST'])
+def add_property():
+    if request.method == 'POST':
+        name = request.form['name']
+        price = request.form['price']
+        address = request.form['address']
+        property_status = request.form['status']
+        area = request.form['area']
+        bathroom = request.form['bathroom']
+        garage = request.form['garage']
+        property_type = request.form['property_type']
+
+        upload_date = datetime.now()
+
+        new_property = Property(
+            name=name, 
+            price=price, 
+            address=address, 
+            upload_date=upload_date,
+            property_status=property_status,
+            area=area,
+            bathroom=bathroom,
+            garage=garage,
+            property_type=property_type,
+            agent_id=1,
+        )
+
+
+        try: 
+            db.session.add(new_property)
+            db.session.commit()
+           
+
+            property_id = new_property.id
+            # # handle image upload
+            # image_data = request.files.get('image')
+           
+            files = request.files.getlist('files[]')
+            #print(files)
+             # Create a folder for each property to organize images
+            property_folder = os.path.join(app.config['UPLOAD_FOLDER'], str(property_id))
+            for image_data in files:
+                if image_data:
+                    result = upload_image(property_id, image_data, property_folder)
+                    print(result)
+                
+            flash('Property added successfully')
+        except Exception as e:
+            db.session.rollback()
+            print(f"Error adding property: {str(e)}")
+            flash('Error adding property. Please try again', 'error')
+    return render_template('add_property.html') 
+
+@app.route('/show_property/<int:property_id>')
+def show_property(property_id):
+    property_data = Property.query.get_or_404(property_id)
+    images = PropertyImage.query.filter_by(property_id=property_id).all()
+    for image in images:
+        current_image_path = image.image_path
+        formatted_path = current_image_path.split("static/", 1)[1]
+        
+
+    return render_template('view_property.html', property_data=property_data, image=image, image_path=formatted_path)
+
+
+def get_mimetype(file_path):
+    return mimetypes.guess_type(file_path)[0] or 'application/octet-stream'
+
+def upload_image(property_id, image_data, property_folder):
+    try:
+        if not image_data:
+            return {"status": "error", "message": "No image data provided."}
+
+        # Get the filename and check if it's allowed
+        filename = secure_filename(image_data.filename)
+        allowed_extensions = {'png', 'jpg', 'jpeg', 'gif'}
+        if '.' not in filename or filename.rsplit('.', 1)[1].lower() not in allowed_extensions:
+            return {"status": "error", "message": "Invalid file type. Allowed types are png, jpg, jpeg, gif."}
+
+       
+        os.makedirs(property_folder, exist_ok=True)
+
+        # Save the image file
+        file_path = os.path.join(property_folder, filename)
+        image_data.save(file_path)
+        now = datetime.now()
+
+        new_image = PropertyImage(
+            name=filename,
+            property_id=property_id,
+            upload_date=now,
+            image_path=file_path,
+            mimetype=get_mimetype(file_path)
+        )
+
+        db.session.add(new_image)
+        db.session.commit()
+        print('=======File uploaded successfully=====')
+
+    except Exception as e:
+        app.logger.error(f"Error uploading image: {str(e)}")
+        return {"status": "error", "message": "An unexpected error occurred during image upload."}
+
 
 
 if __name__=="__main__":
